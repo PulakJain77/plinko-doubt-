0173e49fe143c5ee63d25b9a6f628b296aa82515
PulakJain77 <69963406+PulakJain77@users.noreply.github.com>
2020-11-05 15:04:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(400,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+
+
+for(var i; i = 50 ; i=i+50){
+  var plinko = createSprite(i,50,20,20);
+}
+
+
+
+
+
+
+
+
+
+  drawSprites();z
+}
